d932d9e9d26f2fbc1c9a59f87bdd7b1255cb6f8d
Drew Gordon <drew.s.gordon@gmail.com>
2017-10-23 22:57:46 -0400

    finish it

--- course_1/week3/assn/assn3.scala
+++ course_1/week3/assn/assn3.scala
@@ -18,28 +18,31 @@ sentences.count //64920
 
 val sample = lines.takeSample(false,500,System.nanoTime.toInt)
 sc.makeRDD(sample.toList).saveAsTextFile("hdfs://sandbox.hortonworks.com:8020/tmp/war_n_peace_sample.txt")
-
 // check to make sure we can reload it from hdfs
 val sample_lines = sc.textFile("hdfs://sandbox.hortonworks.com:8020/tmp/war_n_peace_sample.txt")
 
 
 //4. 
 
-val df = spark.sql("SELECT * FROM csv.`hdfs://sandbox.hortonworks.com:8020/tmp/home_data.csv`")
-val res = df.select("_c14 < 1979")
+val df = spark.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load("hdfs://sandbox.hortonworks.com:8020/tmp/home_data.csv")
+val res = df.filter(df("yr_built") < 1979)
 res.count //11991
-
-//maybe try using this for headers: https://www.nodalpoint.com/spark-data-frames-from-csv-files-handling-headers-column-types/
 
 //5.
 
-val res = df.filter("_c6 > (_c5*3)")
+val res = df.filter("sqft_lot > (sqft_living*3)") //change these
 res.count //14267
 res.write.csv("hdfs://sandbox.hortonworks.com:8020/tmp/home_data_large_lots.csv")
 
 //Bonus:
 
 //1.
-
+val zip_df = spark.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load("hdfs://sandbox.hortonworks.com:8020/tmp/wa_zipcodes.csv")
+val join_df = df.join(zip_df, df.col("zipcode") === zip_df.col("Zipcode"))
+val res = join_df.filter(join_df("City")==="Seattle")
+res.count //8977
 
 //2.
+val map_rdd = df.rdd.map(row=>(row.toSeq.head.toString, row.toSeq.tail.toList)) //something like this
+map_rdd.take(3)
+
