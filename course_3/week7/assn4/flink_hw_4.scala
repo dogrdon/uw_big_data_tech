37c419f0b06bc0a1377f72dab653205000c11ffa
Drew Gordon <drew.s.gordon@gmail.com>
2018-05-14 22:51:45 -0400

    hw 4

--- course_3/week7/assn4/flink_hw_4.scala
+++ course_3/week7/assn4/flink_hw_4.scala
@@ -1,30 +1,42 @@
-//flink_hw_4.scala
-
 import org.apache.flink.table.api._
+import org.apache.flink.api.scala._
+import org.apache.flink.table.api.scala._
 import org.apache.flink.table.sources.CsvTableSource
 import org.apache.flink.types.Row
+
+//ref: https://ci.apache.org/projects/flink/flink-docs-release-1.4/dev/table/tableApi.html
+//ref: // https://ci.apache.org/projects/flink/flink-docs-release-1.4/dev/table/sql.html
 
 
 val tEnv = TableEnvironment.getTableEnvironment(benv)
 
-val teenpreg = (CsvTableSource.builder.path("~/data/teen_birth_rates.csv").fieldDelimiter(",")
-					.field("Year", Types.INT)
-					.field("State", Types.STRING)
-					.field("County", Types.STRING)
-					.field("State FIPS Code", Types.STRING)
-					.field("County FIPS Code", Types.STRING)
-					.field("Combined FIPS Code", Types.STRING)
-					.field("Birth Rate", Types.FLOAT)
-					.field("Lower Confidence Limit", Types.FLOAT)
-					.field("Upper Confidence Limit", Types.FLOAT)
+// ignoreFirstLine if header so columns parse: https://ci.apache.org/projects/flink/flink-docs-release-1.4/dev/table/sourceSinks.html
+val tbr_data = (CsvTableSource.builder.path("/root/data/teen_birth_rate.csv").fieldDelimiter(",")
+					.field("yr", Types.INT)
+					.field("state", Types.STRING)
+					.field("county", Types.STRING)
+					.field("state_fips", Types.STRING)
+					.field("county_fips", Types.STRING)
+					.field("combined_fips", Types.STRING)
+					.field("birth_rate", Types.FLOAT)
+					.field("lower_conf_lim", Types.FLOAT)
+					.field("upper_conf_lim", Types.FLOAT)
+					.ignoreFirstLine
 					.build)
 
-teenpreg.getTableSchema
-tEnv.registerTableSource("tpreg", teenpreg)
 
-val highest = tEnv.sqlQuery("select * from tpreg")
-val ds = tp_table.toDataSet[Row]
+tbr_data.getTableSchema
+tEnv.registerTableSource("tbrdata", tbr_data)
 
-// ERROR!: value toDataSet is not a member of org.apache.flink.table.api.Table
+//a. 
+val max_county_yr = tEnv.sqlQuery(s"select county, yr, birth_rate from tbrdata group by county, yr, birth_rate order by birth_rate desc limit 1")
+var max_county_yr_res = max_county_yr.toDataSet[Row]
 
-// https://ci.apache.org/projects/flink/flink-docs-release-1.4/dev/table/sql.html
+max_county_yr_res.print()
+
+
+//b.
+val max_county = tEnv.sqlQuery(s"select t.county, t.birth_rate from tbrdata t join (select MAX(birth_rate) as br from tbrdata) mx on t.birth_rate = mx.br")
+val max_county_res = max_county.toDataSet[Row]
+
+max_county_res.print()
